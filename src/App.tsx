95269324bc67c931e611dae1a6b9fe996f42b296
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2021-11-30 19:34:32 -0300

    Implementando o React Model

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import {useState} from 'react';
 
 function App() {
+
+  Modal.setAppElement('#root');
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModal(true);
+  }
+
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModal(false);
+  }
+
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+
+      <Modal
+            isOpen = {isNewTransactionModalOpen}
+            onRequestClose = {handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
