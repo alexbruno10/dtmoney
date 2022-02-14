22f1788e63d377043516991d1f43635727132059
Alex Bruno <alex_brunoneri@hotmail.com>
2022-02-14 20:57:08 -0300

    Realizando ajustes no Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
-
+import { Container, TransactionTypeContainer } from './styles';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -17,6 +19,10 @@ export default function NewTransactionModal({isOpen, onRequestClose} : NewTransa
         overlayClassName="react-modal-overlay"
         className="react-modal-content"
         >
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={closeImg} alt="Fechar" />
+            </button>
+
             <Container>
             
             <h2>Cadastrar Transação</h2>
@@ -30,6 +36,24 @@ export default function NewTransactionModal({isOpen, onRequestClose} : NewTransa
             type="number"
             placeholder="Valor"
             />
+
+            <TransactionTypeContainer>
+
+            <button
+                type="button"
+            >
+                <img src={incomeImg} alt="Entrada" />
+                <span>Entrada</span>
+            </button>
+
+            <button
+                type="button"
+            >
+                <img src={outcomeImg} alt="Saída" />
+                <span>Saída</span>
+            </button>
+
+            </TransactionTypeContainer>
 
             <input 
             placeholder="Categoria"
