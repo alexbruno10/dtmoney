5c541de283ade20d013fce1da49c88a34b805a3e
Alex Bruno <alex_brunoneri@hotmail.com>
2022-02-16 20:20:47 -0300

    Criação do Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import Modal from 'react-modal';
 import {useState} from 'react';
 import NewTransactionModal from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 function App() {
 
@@ -21,7 +22,7 @@ function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}> 
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
@@ -29,7 +30,7 @@ function App() {
       />
       <Dashboard />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
 
